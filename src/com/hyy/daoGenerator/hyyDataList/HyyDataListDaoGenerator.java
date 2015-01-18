4a9587f8637734455d1bf25caadad016a0aeb4f1
kevin huang <hyy12345678@gmail.com>
2015-01-18 23:20:14 +0800

    fix alarm Entity
    change alarm’s messageId to Long type,add “add to One” relation to it.

--- src/com/hyy/daoGenerator/hyyDataList/HyyDataListDaoGenerator.java
+++ src/com/hyy/daoGenerator/hyyDataList/HyyDataListDaoGenerator.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import de.greenrobot.daogenerator.DaoGenerator;
 import de.greenrobot.daogenerator.Entity;
+import de.greenrobot.daogenerator.Property;
 import de.greenrobot.daogenerator.Schema;
 
 public class HyyDataListDaoGenerator {
@@ -16,35 +17,60 @@ public class HyyDataListDaoGenerator {
 	public static void main(String[] args) throws IOException, Exception {
 		// TODO Auto-generated method stub
 		Schema schema = new Schema(1, "com.hyy.hyydatalist.generator");
-		addMessage(schema);
-		addAlarm(schema);
+//		addMessage(schema);
+//		addAlarm(schema);
+		
+		addMessageAndAlarm(schema);
 		
 		new DaoGenerator().generateAll(schema, "../hyyDataList/src-gen");
 	}
 
 	
-	private static void addMessage(Schema schema) {
-		// TODO Auto-generated method stub
+	private static void addMessageAndAlarm(Schema schema){
+		
+		//Entity for message
 		Entity messages = schema.addEntity("Messages");
 		messages.addIdProperty();
 		messages.addStringProperty("title");
 		messages.addStringProperty("shortcut");
 		messages.addStringProperty("content");
-		messages.addStringProperty("alarmstatus");
+		messages.addStringProperty("alarmstatus").getProperty();
 		
-	}
-	
-	private static void addAlarm(Schema schema) {
-		// TODO Auto-generated method stub
+		//Entity for alarm
 		Entity alarms = schema.addEntity("Alarms");
 		alarms.addIdProperty();
 		alarms.addStringProperty("alarmtime");
 		alarms.addStringProperty("dayofweek");
-		alarms.addStringProperty("messageid");
+//		alarms.addStringProperty("messageid");
+		Property messageIdProperty = alarms.addLongProperty("messageid").getProperty();
+		alarms.addToOne(messages, messageIdProperty);
+		
 		alarms.addStringProperty("ispause");
 		
-		
 	}
+	
+	// private static void addMessage(Schema schema) {
+	// // TODO Auto-generated method stub
+	// Entity messages = schema.addEntity("Messages");
+	// messages.addIdProperty();
+	// messages.addStringProperty("title");
+	// messages.addStringProperty("shortcut");
+	// messages.addStringProperty("content");
+	// messages.addStringProperty("alarmstatus").getProperty();
+	//
+	// }
+	//
+	// private static void addAlarm(Schema schema) {
+	// // TODO Auto-generated method stub
+	// Entity alarms = schema.addEntity("Alarms");
+	// alarms.addIdProperty();
+	// alarms.addStringProperty("alarmtime");
+	// alarms.addStringProperty("dayofweek");
+	// alarms.addStringProperty("messageid");
+	// alarms.addStringProperty("ispause");
+	//
+	//
+	// }
 
 
 }
